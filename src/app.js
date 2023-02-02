f5db9eb342fc1df81b5d088aab58f2b8e91d8baa
Gabriel castelo branco <gabriel.pdg8@gmail.com>
2023-02-02 04:51:21 -0300

    commit rodando servers

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+app.listen(5000, () => console.log(`Server rodando na porta 5000`))
